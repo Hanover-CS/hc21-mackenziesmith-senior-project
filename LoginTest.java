08e308b1bb60aee1a3bb472e38ef2b09c0309a8b
Mackenzie Smith <45366095+smithma21@users.noreply.github.com>
2021-01-31 21:12:31 -0500

    Update LoginTest.java
    added comments and updated asserts

--- LoginTest.java
+++ LoginTest.java
@@ -3,6 +3,7 @@ package tests;
 import functions.CommonFunctions;
 import functions.PageLogin;
 import functions.PageUserHome;
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -17,23 +18,39 @@ public class LoginTest {
     @Test
     public void loginAsAdmin() throws InterruptedException {
         ChromeDriver driver = CommonFunctions.initDriver();
+
+        // Create login page object and navigate to page
         PageLogin objLogin = new PageLogin(driver);
         Assert.assertEquals(objLogin.getPageHeader(), "Faculty elections");
+
         objLogin.enterUsername(usernameAdmin);
         objLogin.enterPassword(passwordAdmin);
         objLogin.clickLoginBtn();
+
+        // Assert login successful
+        Assert.assertTrue(driver.findElement(By.xpath("//a[contains(text(),'Logout')]")).isDisplayed());
+
         driver.quit();
     }
 
     @Test
     public static void loginAsStandardUser() throws InterruptedException {
         ChromeDriver driver = CommonFunctions.initDriver();
+
+        // Create login page object and navigate to page
         PageLogin objLogin = new PageLogin(driver);
+        Assert.assertEquals(objLogin.getPageHeader(), "Faculty elections");
+
         objLogin.enterUsername(usernameUser);
         objLogin.enterPassword(passwordUser);
         objLogin.clickLoginBtn();
+
+        // Create user homepage object to get page header
         PageUserHome objUserHome = new PageUserHome(driver);
+
+        // Assert login successful by getting page header
         Assert.assertEquals(objUserHome.getPageHeader(), "Current Elections");
+
         driver.quit();
     }
 
